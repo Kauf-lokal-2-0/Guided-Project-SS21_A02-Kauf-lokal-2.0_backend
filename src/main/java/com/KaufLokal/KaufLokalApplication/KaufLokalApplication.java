024a01669ca4bacfee46ef6b4c3095d7f6d10fe0
MHallweger <39130286+MHallweger@users.noreply.github.com>
2021-05-27 18:42:54 +0200

    Add files via upload

---
+++ src/main/java/com/KaufLokal/KaufLokalApplication/KaufLokalApplication.java
@@ -0,0 +1,13 @@
+package com.KaufLokal.KaufLokalApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaufLokalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaufLokalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KaufLokal/KaufLokalApplication/KaufLokalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KaufLokal.KaufLokalApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaufLokalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
